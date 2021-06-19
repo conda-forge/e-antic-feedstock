4d51dd4ccd86d128ab028fd43e005150cda93597
Julian Rüth <julian.rueth@fsfe.org>
2021-06-19 15:10:37 +0200

    build pyeantic and libeantic

---
+++ recipe/build-libeantic.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
+set -x
+
+if [[ "$target_platform" == osx* ]]; then
+    CXXFLAGS="$CXXFLAGS -fno-common"
+fi
+
+if [[ "$target_platform" == win* ]]; then
+    cp $PREFIX/lib/gmp.lib $PREFIX/lib/gmpxx.lib
+    CXXFLAGS="$CXXFLAGS -std=c++14"
+fi
+
+set +x
+
+cd libeantic
+
+./configure --prefix="$PREFIX" --without-benchmark --without-byexample --without-version-script || (cat config.log; false)
+[[ "$target_platform" == "win-64" ]] && patch_libtool
+
+make -j${CPU_COUNT}
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+make check || (cat test-suite.log; false)
+fi
+make install
+
